21089c3f9eb01f75e74f906cb3abf329dcc034d9
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2024-09-23 11:35:18 +0300

    render home page

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('home')
 })
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
