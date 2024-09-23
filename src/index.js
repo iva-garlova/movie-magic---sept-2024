158a7469f84f02d4e6ed53ffdb32aaeb5717ea54
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2024-09-23 10:17:28 +0300

    setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('it works!!')
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Just works!')
+})
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
