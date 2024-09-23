b97a8d52d31af55fccb3576ffc14dde6469cc98b
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2024-09-23 16:14:21 +0300

    add bodyparcer middleware

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-
+app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(routes);
 
