77078db99aac139689a08b95ff24770697fec320
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2024-09-23 16:42:22 +0300

    create movie functionaloty

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,9 +1,18 @@
+import uniqid from 'uniqid';
+
 import movieData from "../data/movieData.js";
 
    
-    const getAll = () => movieData.getMovies();
+    const getAll = () => movieData.getAll();
+
+    const create = (movie) =>{
+        movie.id = uniqid();
+
+        return movieData.create(movie);
+    };
 
 
     export default {
         getAll,
+        create
     }
